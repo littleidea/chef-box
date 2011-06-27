ea5d74996b8a6f6e2381ef93d851b6916c2104d6
Tim Dysinger <tim@dysinger.net>
2011-06-27 07:09:12 -1000

    Sloppy regex - my bad

--- scripts/chef-client.sh
+++ scripts/chef-client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
 # chef client
-perl -p -i -e 's/127\.0\.0.\1/172.16.172.16/g' /etc/chef/client.rb
+perl -p -i -e 's/127\.0\.0\.1/172\.16\.172\.16/g' /etc/chef/client.rb
 invoke-rc.d chef-client start
 update-rc.d chef-client defaults
