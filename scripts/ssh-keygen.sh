4e60dae53dc9cca1ad3adb49ccda1c05ed865165
Tim Dysinger <tim@dysinger.net>
2011-06-26 14:25:15 -1000

    Added new ssh key to known hosts

--- scripts/ssh-keygen.sh
+++ scripts/ssh-keygen.sh
@@ -3,5 +3,6 @@
 # ssh keygen
 chown -R vagrant:vagrant ~/.ssh
 sudo -u vagrant ssh-keygen -q -N "" -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod go-rwsx ~/.ssh
 rsync -vauc ~/.ssh/ /vagrant/.ssh/
