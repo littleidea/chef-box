7359bcc0160bf9a0b6edf6c70fef141b6d3faa30
Tim Dysinger <tim@dysinger.net>
2011-06-27 11:45:29 -1000

    Use /home/vagrant instead of ~ & also add chmod o-rwx to ~/.chef

--- scripts/knife-client.sh
+++ scripts/knife-client.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 
 # knife
-if [ ! -f ~/.chef/knife.rb ]; then
+if [ ! -f /home/vagrant/.chef/knife.rb ]; then
     usermod -a -G chef vagrant
     chmod g+r /etc/chef/{validation,webui}.pem
-    sudo -u vagrant knife configure -i -n -r ~ --defaults
+    sudo -u vagrant knife configure -i -n -r /home/vagrant --defaults
     echo '
 dir = File.dirname(__FILE__)
 log_level :debug
@@ -15,6 +15,7 @@ chef_server_url "http://172.16.172.16:4000"
 cache_type "BasicFile"
 cache_options :path => "#{dir}/checksums"
 cookbook_path ["#{dir}/../cookbooks", "/vagrant"]
-' | sudo -u vagrant tee ~/.chef/knife.rb
-    rsync -r ~/.chef/ /etc/chef/validation.pem /vagrant/.chef/
+' | sudo -u vagrant tee /home/vagrant/.chef/knife.rb
+    chmod -R o-rwx /home/vagrant/.chef
+    rsync -vac /home/vagrant/.chef/ /etc/chef/validation.pem /vagrant/.chef/
 fi
